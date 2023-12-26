f1f73fbdd5bfee8e108dbf256dffc68dd0233327
Renato Valhe <146896705+Renatodev011@users.noreply.github.com>
2023-12-26 20:06:21 -0300

    Modificado

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-const form = document.getElementById('formulario');
-
-let Validform = false 
-
-function numberVal (A,B){
-    return B > A;
-}
-
-form.addEventListener('buttom', function(e) {
-    e.defaultPrevented();
-
-    let A = parseFloat(document.getElementById('ValorA').value);
-    let B = parseFloat(document.getElementById('ValorB').value);
-
-    const messagesucess = 'Formulario enviado!';
-    const containermessagesucess = document.querySelector('Sucesso');
-
-    formValid = numberVal(A,B);
-
-    if (formValid){
-        document.querySelector('error-message').style.display = 'none';
-        containermessagesucess.innerHTML = messagesucess;
-        containermessagesucess.style.display = 'block';
-        document.querySelector('.error-message').style.display = 'none';
-
-        A.value = '';
-        B.value = '';
-
-    } else {
-        document.querySelector('.error-message').style.display = 'block';
-        containermessagesucess.style.display = 'none';
+const form = document.getElementById('form-val');
+    
+    function numberVal (A,B){
+        return B > A;
     }
-})
+    
+    form.addEventListener('btn', function(e) {
+        e.defaultPrevented();
+    
+        let A = parseFloat(document.getElementById('valorA').value);
+        let B = parseFloat(document.getElementById('valorB').value);
+        let formValido = false;
+        const mensagemsucesso = 'O valor B(<b>${numeroTotalB}<b>) é maior que A(<b>${numeroTotalA}<b>)';
+        const mensagemError = 'O valor B(<b>${numeroTotalB}<b>) é menor que A(<b>${numeroTotalA}<b>)';
+        function validação(numeroTotalA, numeroTotalB){
+            return numeroTotalA < numeroTotalB
+        }
+
+            formValido = numberVal(A,B);
+        if (formValido){
+            document.querySelector('error-message').style.display = 'none';
+            containermessagesucess.innerHTML = messagesucess;
+            containermessagesucess.style.display = 'block';
+            document.querySelector('.error-message').style.display = 'none';
+    
+            A.value = '';
+            B.value = '';
+    
+        } 
+        else {
+            document.querySelector('.error-message').style.display = 'block';
+            document.querySelector('.success-message').style.display = 'none';
+        }
+    })
