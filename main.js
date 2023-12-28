99894ad4872ccc4a3a806380e4e3fc97d7c87d95
Renato Valhe <146896705+Renatodev011@users.noreply.github.com>
2023-12-28 19:09:47 -0300

    Reparo

--- main.js
+++ main.js
@@ -4,22 +4,19 @@ const form = document.getElementById('form-val');
         return B > A;
     }
     
-    form.addEventListener('btn', function(e) {
-        e.defaultPrevented();
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
     
         let A = parseFloat(document.getElementById('valorA').value);
         let B = parseFloat(document.getElementById('valorB').value);
         let formValido = false;
         const mensagemsucesso = 'O valor B(<b>${numeroTotalB}<b>) é maior que A(<b>${numeroTotalA}<b>)';
         const mensagemError = 'O valor B(<b>${numeroTotalB}<b>) é menor que A(<b>${numeroTotalA}<b>)';
-        function validação(numeroTotalA, numeroTotalB){
-            return numeroTotalA < numeroTotalB
-        }
-
+        
             formValido = numberVal(A,B);
         if (formValido){
             document.querySelector('error-message').style.display = 'none';
-            containermessagesucess.innerHTML = messagesucess;
+            containermessagesucess.innerHTML = mensagemsucesso;
             containermessagesucess.style.display = 'block';
             document.querySelector('.error-message').style.display = 'none';
     
